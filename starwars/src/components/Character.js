efb729f537f6ed98896569185102e6bcb8fd7eba
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-09-13 14:40:53 +0100

    map over array to return several characters

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 
-function Character(props){
-    const {name, birth_date, gender, eye_color, hair_color} = props;
+function Character(props) {
+  const { name, birth_date, gender, eye_color, hair_color } = props;
 
-    return(
-        <div>
-           <h3>Name: {name}</h3>
-           <p>Date of Birth: {birth_date}</p>
-           <p>Gender: {gender}</p>
-           <p>Eye Colour: {eye_color}</p>
-           <p>Hair Colour: {hair_color}</p>
-
-        </div>
-    )
+  return (
+    <div>
+      <h3>Name: {name}</h3>
+      <p>Date of Birth: {birth_date}</p>
+      <p>Gender: {gender}</p>
+      <p>Eye Colour: {eye_color}</p>
+      <p>Hair Colour: {hair_color}</p>
+    </div>
+  );
 }
+
+export default Character;
--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React from "react";
+import Character from './Character';
 
-function CharacterList(props){
-    console.log(props);
-    return('Hello World');
-};
+function CharacterList(props) {
+    return(
+    props.characterArray.map((character) => (
+    <Character
+      key={character.name}
+      name={character.name}
+      birth_date={character.birth_date}
+      gender={character.gender}
+      eye_color={character.eye_color}
+      hair_color={character.hair_color}
+    />
+    )
+
+  ));
+}
 
 export default CharacterList;
